94dfa9c18e5963dc4c9a4836a36a66517e4efd11
Thanmai Bitla <2303a52126@sru.edu.in>
2026-06-04 11:45:04 +0530

    Create Login.js

---
+++ backend/src/Login.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const res = await axios.post(
+      "http://localhost:5000/api/auth/login",
+      { email, password }
+    );
+
+    localStorage.setItem("token", res.data.token);
+
+    alert("Login Success");
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+export default Login;

8cdd3f4e2a43a9ca16e6e0f7ddf5ebd753c6843c
Thanmai Bitla <2303a52126@sru.edu.in>
2026-06-04 11:45:57 +0530

    Create Dashboard component for InterviewAI Pro

---
+++ backend/src/Dashboard.js
@@ -0,0 +1,15 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>InterviewAI Pro Dashboard</h1>
+
+      <h3>Total Questions Practiced: 25</h3>
+
+      <h3>Coding Problems Solved: 10</h3>
+
+      <h3>Progress: 60%</h3>
+    </div>
+  );
+}
+
+export default Dashboard;
